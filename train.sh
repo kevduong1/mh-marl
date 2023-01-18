6279c7d15488d88cd86c6461770bbddc9b6dc27a
kevduong1 <kevduong@ksu.edu>
2023-01-17 18:59:57 -0600

    req text

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wandb docker-run --gpus device=0 -ti mh-marl:1.0 \
-    --config=mappo_ns --env-config=gymma with \
-    hidden_dim=128 lr=0.0005 entropy_coef=0.001 target_update_interval_or_tau=0.01 \
-    env_args.time_limit=500 t_max=5000000 env_args.key="rware:rware-tiny-2ag-v1" \
-    wandb_args.project="mh-v1" wandb_args.tag="baseline" wandb_args.group="single" seed=10002
+    --config=iql_ns --env-config=pettingzoo with \
+    epsilon_anneal_time=5000000 \
+    hidden_dim=128 lr=0.0005 target_update_interval_or_tau=0.01 \
+    env_args.time_limit=500 t_max=10000000 env_args.key="None" \
+    wandb_args.project="mh-v1" wandb_args.tag="idql_ns" wandb_args.group="kaz" seed=10002
